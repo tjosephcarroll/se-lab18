7126ae213b65c64fc1f08b6ac376543f2e2eb8b8
Luca <luca@intersystems.com>
2018-09-05 06:13:21 +0000

    removed the 'SE' string

--- container/lab1/server.js
+++ container/lab1/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello SE World!");
+  response.end("Hello World!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
