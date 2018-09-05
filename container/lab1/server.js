176841223e37614655b53340aa1edb3f9793dd1f
Luca <luca@intersystems.com>
2018-09-05 06:06:09 +0000

    add SE to Hello string and test  gitpod

--- container/lab1/server.js
+++ container/lab1/server.js
@@ -1,8 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World!");
+  response.end("Hello SE World!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
-
